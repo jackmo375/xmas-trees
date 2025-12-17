113f8bb92dc22d7b8618b040d847a34bcdfb1fcf
Jack Morrice <jackmo375@gmail.com>
2025-12-17 02:11:39 +0200

    parallelized it!

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import cma
 import math
 import random
 from functools import partial
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
 def main():
 
-    N_trees = 20
-    N_tries = 3
+    N_trees = 200
+    N_tries = 20
     radius = 20.0
     sigma0 = 3
 
@@ -19,18 +20,32 @@ def main():
 
     objective_for_cma = partial(xt.constrained_objective, placed_trees=placed_trees)
 
+    optimize_objective = partial(
+        cma.fmin2,
+        objective_for_cma,
+        sigma0 = sigma0,
+        options={'verbose': -9})
+
     for i in range(N_trees-1):
-        best_x = None
-        best_cost_value = 1e20
+        initial_conditions = []
         for _ in range(N_tries):
             angle = xt.generate_weighted_angle()
             vx = math.cos(angle)
             vy = math.sin(angle)
             x0 = [radius * vx,radius * vy,random.uniform(0, 2*math.pi)]
-            x, es = cma.fmin2(objective_for_cma, x0, sigma0, options={'verbose': -9})
+            initial_conditions.append(x0)
+
+        with ProcessPoolExecutor() as executor:
+            results = list(executor.map(optimize_objective, initial_conditions))
+
+        best_x = None
+        best_cost_value = 1e20
+        for tuple in results:
+            x = tuple[0]
             if (objective_for_cma(x) < best_cost_value):
                 best_cost_value = objective_for_cma(x)
                 best_x = x
+
         print(i)
         print(best_x)
         print(objective_for_cma(best_x))
